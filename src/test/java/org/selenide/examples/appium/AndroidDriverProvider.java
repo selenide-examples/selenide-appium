00e2dc8d8c0b23817a3c9a98a4e535f366ee1777
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-10-30 01:28:30 +0300

    upgrade to appium 4.1.2

--- src/test/java/org/selenide/examples/appium/AndroidDriverProvider.java
+++ src/test/java/org/selenide/examples/appium/AndroidDriverProvider.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_ACTIVITY;
+import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_PACKAGE;
+
 public class AndroidDriverProvider implements WebDriverProvider {
     @Override
     public WebDriver createDriver(DesiredCapabilities capabilities) {
@@ -16,8 +19,8 @@ public class AndroidDriverProvider implements WebDriverProvider {
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("deviceName", "0123456789ABCDEF");
-        capabilities.setCapability(MobileCapabilityType.APP_PACKAGE, "com.android.calculator2"); // This is package name of your app (you can get it from apk info app
-        capabilities.setCapability(MobileCapabilityType.APP_ACTIVITY, "com.android.calculator2.Calculator"); // This is Launcher activity of your app (you can get it from apk info app)
+        capabilities.setCapability(APP_PACKAGE, "com.android.calculator2"); // This is package name of your app (you can get it from apk info app
+        capabilities.setCapability(APP_ACTIVITY, "com.android.calculator2.Calculator"); // This is Launcher activity of your app (you can get it from apk info app)
         //Create AndroidDriver instance and connect to the Appium server.
         //It will launch the Calculator App in Android Device using the configurations specified in Desired Capabilities
 
