4d86fe095a1e19871e06b0994a443c4f348d502f
Andrei Solntsev <andrei.solntsev@gmail.com>
2019-06-18 01:36:43 +0300

    avoid using  setWebDriver() and quit() manually

--- src/test/java/org/selenide/examples/appium/AbstractApiDemosTest.java
+++ src/test/java/org/selenide/examples/appium/AbstractApiDemosTest.java
@@ -1,27 +1,22 @@
 package org.selenide.examples.appium;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.close;
+import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public abstract class AbstractApiDemosTest {
   @Before
   public void setUp() {
     close();
+    Configuration.startMaximized = false;
+    Configuration.browserSize = null;
     Configuration.browser = AndroidDriverWithDemos.class.getName();
-    WebDriverRunner.setWebDriver(new AndroidDriverWithDemos().createDriver(new DesiredCapabilities()));
-  }
-
-  @After
-  public void tearDown() {
-    getWebDriver().quit();
+    open("/");
   }
 
   @SuppressWarnings("unchecked")
--- src/test/java/org/selenide/examples/appium/CalculatorTest.java
+++ src/test/java/org/selenide/examples/appium/CalculatorTest.java
@@ -1,42 +1,36 @@
 package org.selenide.examples.appium;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.close;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static com.codeborne.selenide.Selenide.open;
 
 /**
  * Thanks to [Maryna Kolesnik](https://github.com/mkpythonanywhereblog) for this example!
- * Her original sample can be found [here](https://gist.github.com/mkpythonanywhereblog/d1fb3dca2e66146f519f) 
+ * Her original sample can be found [here](https://gist.github.com/mkpythonanywhereblog/d1fb3dca2e66146f519f)
  */
 public class CalculatorTest {
 
-    @Before
-    public void setUp() {
-        close();
-        Configuration.browser = AndroidDriverWithCalculator.class.getName();
-        WebDriverRunner.setWebDriver(new AndroidDriverWithCalculator().createDriver(new DesiredCapabilities()));
-    }
+  @Before
+  public void setUp() {
+    close();
+    Configuration.startMaximized = false;
+    Configuration.browserSize = null;
+    Configuration.browser = AndroidDriverWithCalculator.class.getName();
+    open("/");
+  }
 
-    @After
-    public void tearDown() {
-        getWebDriver().quit();
-    }
-
-    @Test
-    public void calculator(){
-        $(By.id("digit_2")).click();
-        $(By.id("op_add")).click();
-        $(By.id("digit_4")).click();
-        $(By.id("eq")).click();
-        $(By.id("result")).shouldHave(text("6"));
-    }
+  @Test
+  public void calculator() {
+    $(By.id("digit_2")).click();
+    $(By.id("op_add")).click();
+    $(By.id("digit_4")).click();
+    $(By.id("eq")).click();
+    $(By.id("result")).shouldHave(text("6"));
+  }
 }
