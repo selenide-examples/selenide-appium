327bc62e7d5f695883b92358cebd5fcb91121c77
Andrei Solntsev <andrei.solntsev@gmail.com>
2019-06-18 01:06:04 +0300

    upgrade to Selenide 5.x
    implementation is not ideal yet - we need to call setWebDriver() and quit() manually.
    Hope to improve it later.

--- src/test/java/org/selenide/examples/appium/CalculatorTest.java
+++ src/test/java/org/selenide/examples/appium/CalculatorTest.java
@@ -1,12 +1,17 @@
 package org.selenide.examples.appium;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.close;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 /**
  * Thanks to [Maryna Kolesnik](https://github.com/mkpythonanywhereblog) for this example!
@@ -16,7 +21,14 @@ public class CalculatorTest {
 
     @Before
     public void setUp() {
+        close();
         Configuration.browser = AndroidDriverWithCalculator.class.getName();
+        WebDriverRunner.setWebDriver(new AndroidDriverWithCalculator().createDriver(new DesiredCapabilities()));
+    }
+
+    @After
+    public void tearDown() {
+        getWebDriver().quit();
     }
 
     @Test
