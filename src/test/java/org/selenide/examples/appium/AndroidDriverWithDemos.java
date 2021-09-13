b6870ba812d3728bc862a2738dd10c13bc5e6434
Andrei Solntsev <andrei.solntsev@gmail.com>
2021-09-13 23:22:10 +0300

    upgrade to selenide:5.24.3 and selenide-appium:1.6.9

--- src/test/java/org/selenide/examples/appium/AndroidDriverWithDemos.java
+++ src/test/java/org/selenide/examples/appium/AndroidDriverWithDemos.java
@@ -6,6 +6,8 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,6 +18,8 @@ import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
 
 public class AndroidDriverWithDemos implements WebDriverProvider {
     @Override
+    @CheckReturnValue
+    @Nonnull
     public WebDriver createDriver(DesiredCapabilities capabilities) {
         File app = downloadApk();
         capabilities.setCapability(MobileCapabilityType.VERSION, "4.4.2");
@@ -27,7 +31,7 @@ public class AndroidDriverWithDemos implements WebDriverProvider {
         capabilities.setCapability("appActivity", ".ApiDemos");
 
         try {
-            return new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+            return new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
