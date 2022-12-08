f5f0bd552b012c961094ad58e80da4e63a23f9c1
Andrei Solntsev <andrei.solntsev@gmail.com>
2022-12-08 19:05:05 +0200

    bump selenide-appium from 2.3.0 to 2.4.0

--- src/test/java/org/selenide/examples/appium/AndroidDriverWithCalculator.java
+++ src/test/java/org/selenide/examples/appium/AndroidDriverWithCalculator.java
@@ -3,7 +3,6 @@ package org.selenide.examples.appium;
 import com.codeborne.selenide.WebDriverProvider;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.remote.AutomationName;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 
@@ -13,8 +12,6 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
-
-import static org.openqa.selenium.remote.CapabilityType.APPLICATION_NAME;
 
 @ParametersAreNonnullByDefault
 public class AndroidDriverWithCalculator implements WebDriverProvider {
